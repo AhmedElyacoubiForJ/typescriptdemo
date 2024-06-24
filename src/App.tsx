bcd0cbcb51cb7aff3395169e9eee4214aa28c68b
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-24 18:41:35 +0200

    functions w. typescipt

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import "./App.css";
 import Person, {Continent} from "./components/Person";
 
 function App() {
+
+  const getAge  = (birthYear : Date) => {
+    const today = new Date();
+    const age = today.getFullYear() - birthYear.getFullYear();
+    return age;
+  }
+  const birthDay = new Date(1967, 3, 15);
+  //const age = getAge(birthDay);
+  //console.log(age);
   return (
     <div className="App">
       <Person
         name="Mike Smith"
         email="mike.smith@gmail.com"
-        age={45}
+        age={getAge(birthDay)}
         isMarried={true}
         friends={["John", "Jeff", "Mary"]}
         continent={Continent.NORTH_AMERICA}
+        //country="USA" to demonstrate country as optional
       />
     </div>
   );
