e65da5c669cc942c0234f2625487fbf0c13a09d9
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-06-24 17:49:21 +0200

    useState w. typescript

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,23 +1,32 @@
+import { useState } from "react";
 interface Props {
-    name: string,
-    email: string,
-    age: number,
-    isMarried: boolean,
-    friends: Array<string> // string[]
+  name: string;
+  email: string;
+  age: number;
+  isMarried: boolean;
+  friends: Array<string>; // string[]
 }
 
-const Person = (props : Props) => {
+const Person = (props: Props) => {
+  // howto typescript w. useState
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
+  const [isMarried, setIsMarried] = useState<boolean>(false);
+  const [friends, setFriends] = useState<string[]>([]);
+
+
   return (
     <div>
       <h1>Name: {props.name}</h1>
       <h1>Email: {props.email}</h1>
       <h1>Age: {props.age}</h1>
-      <h1>This person {props.isMarried ? 'is' : 'is not'} MARRIED</h1>
-      {props.friends.map( (friend : string) => (
+      <h1>This person {props.isMarried ? "is" : "is not"} MARRIED</h1>
+      {props.friends.map((friend: string) => (
         <h1 key={friend}>{friend}</h1>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Person
+export default Person;
